641ffe5bc792ed21a5c384525387c153e0dd6c5c
kannojiyaamit <123285320+kannojiyaamit@users.noreply.github.com>
2023-12-09 23:01:58 +0000

    conversion datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,9 @@
+let score = null;
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+
+let booleanIs = 3;
+let booleanIsLoggedIn = Boolean(booleanIs)
+console.log(booleanIsLoggedIn)
