d85af74666d360a467079d8705b4b4939f630f41
kannojiyaamit <123285320+kannojiyaamit@users.noreply.github.com>
2023-12-09 22:39:51 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+ const accountId = 144553
+ let accountEmail = "amit_kannojiya12@yahoo.com"
+ var accountPassword = "1234"
+ accountCity = "dehradun"
+ let accountState;
+
+//  accountId =2
+ console.log(accountId)
+  accountEmail ="kannojiyaamit.com"
+  accountPassword= "22112"
+  accountCity ="delhi"
+
+  console.log(accountId)
+  console.table([accountId, accountEmail, accountPassword,accountCity ,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict";// treat as js code as new newer version
+//alert (3 + 3);// we using nodejs,not browser
+
+let name = "amit";
+console.log (typeof  null)
