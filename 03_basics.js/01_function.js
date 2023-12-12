f51baf0cc307fc1282aaed630164a65e4e89d054
kannojiyaamit <123285320+kannojiyaamit@users.noreply.github.com>
2023-12-12 21:05:58 +0000

    function1

---
+++ 03_basics.js/01_function.js
@@ -0,0 +1,34 @@
+
+
+// function addTwonumber(num1,num2){
+//     return num1 + num2;
+
+// }
+//   const result = addTwonumber(3,5)      
+// console.log( "result: ", result);
+
+
+function isloggedIn (username){
+    return `${username} is logged in`;
+}
+//  console.log(isloggedIn("amit"))
+
+ function calculateCartprice(val1, val2, ...num1){
+    return num1
+ }
+//  console.log(calculateCartprice(200,300,400, 500,600))
+
+const user ={
+    username: "amit",
+    price: 199
+}
+function handleuser(userObject){
+    // console.log(`my name is ${userObject.username} and my bottle price is ${userObject.price}`)
+}
+handleuser(user)
+
+const myNewArr =[200, 300, 400, 100]
+function returnSecondArr (value){
+  return value
+}
+  console.log(returnSecondArr(myNewArr))
