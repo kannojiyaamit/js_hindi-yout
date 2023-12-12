6c8213de2cd8d41910ad76345215db0a46c50a03
kannojiyaamit <123285320+kannojiyaamit@users.noreply.github.com>
2023-12-12 20:07:48 +0000

    object json

---
+++ 02_basics.js/04__objects.js
@@ -0,0 +1,19 @@
+// const tinder = new Object()
+const tinderUser = {}
+
+// console.log(tin
+// derUser);
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor: "hitesh"
+}
+const {courseInstructor:amit} = course
+console.log(amit)
+
+{
+    "coursename" : "js in hindi",
+    "price" : "999",
+    "courseInstructor": "hitesh"
+}
