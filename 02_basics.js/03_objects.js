8b5b44cfb486072f5b3f69ea8e7bc8732f69aeae
kannojiyaamit <123285320+kannojiyaamit@users.noreply.github.com>
2023-12-12 20:09:44 +0000

    update

--- 02_basics.js/03_objects.js
+++ 02_basics.js/03_objects.js
@@ -3,5 +3,17 @@
 
 //object literals
 
-const juser 
+const  mySym = Symbol("key1")
+const juser = {
+    name:"hitesh",
+    age :18,
+   [mySym]:"mykey1",
+    Email:"amit_kannojiya@yahoo.com",
+    isLoggedIn:false,
+    lastLoginDays : ["monday","tuesday"]
+}
 
+console.log(juser.Email)
+console.log(juser["Email"])
+console.log(typeof juser[mySym])
+console.log(this)
